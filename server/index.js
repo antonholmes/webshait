0c6c4584cdae55a77cacd7ef7c86b58d04ca0a05
Anton Holmes <holmesanton@icloud.com>
2018-08-08 03:21:48 -0400

    fix(semis): removed semicolons

--- server/index.js
+++ server/index.js
@@ -1,43 +1,43 @@
-import path from 'path';
-import express from 'express';
-import morgan from 'morgan';
-import compression from 'compression';
-import session from 'express-session';
-import socketio from 'socket.io';
-import { userInfo } from 'os';
+import path from 'path'
+import express from 'express'
+import morgan from 'morgan'
+import compression from 'compression'
+import session from 'express-session'
+import socketio from 'socket.io'
+import { userInfo } from 'os'
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const db = require('./db');
-const sessionStore = new SequelizeStore({ db });
-const PORT = process.env.PORT || 4321;
-const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const db = require('./db')
+const sessionStore = new SequelizeStore({ db })
+const PORT = process.env.PORT || 4321
+const app = express()
 
-module.exports = app;
+module.exports = app
 
 if (process.env.NODE_ENV === 'test') {
-  after('close the session store', () => sessionStore.stopExpiringSessions());
+  after('close the session store', () => sessionStore.stopExpiringSessions())
 }
 
-if (process.env.NODE_ENV === 'production') require('../secrets');
+if (process.env.NODE_ENV === 'production') require('../secrets')
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => done(null, user.id))
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.models.user.findById(id);
-    done(null, user);
+    const user = await db.models.user.findById(id)
+    done(null, user)
   } catch (err) {
-    done(err);
+    done(err)
   }
 });
 
 const createApp = () => {
   // Logging
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
   // Body Parsing
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
   // Compression
-  app.use(compression());
+  app.use(compression())
   // Session with Passport
   app.use(
     session({
@@ -46,55 +46,55 @@ const createApp = () => {
       resave: false,
       saveUninitialized: false,
     })
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
   // Auth and API routes
-  app.use('/auth', require('./auth'));
-  app.use('/api', require('./api'));
+  app.use('/auth', require('./auth'))
+  app.use('/api', require('./api'))
   // Static File-Serving
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(express.static(path.join(__dirname, '..', 'public')))
   // Request Error Handling
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error('Not found');
-      err.status = 404;
-      next(err);
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
     } else {
-      next();
+      next()
     }
   });
   // Index.html
   app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-  });
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
   // Error Handling Endware
   app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Internal server error.');
-  });
-};
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
 
 const startListening = () => {
   const server = app.listen(PORT, () =>
     console.log(`Site served on port ${PORT}`)
-  );
-  const io = socketio(server);
-  require('./socket')(io);
-};
+  )
+  const io = socketio(server)
+  require('./socket')(io)
+}
 
-const syncDb = () => db.sync();
+const syncDb = () => db.sync()
 
 async function bootApp() {
-  await sessionStore.sync();
-  await syncDb();
-  await createApp();
-  await startListening();
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
 }
 
 if (require.main === module) {
-  bootApp();
+  bootApp()
 } else {
-  createApp();
+  createApp()
 }
