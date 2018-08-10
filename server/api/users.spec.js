ba816e18966187beffd02ccbc0cd9b42ebce20e1
Anton Holmes <holmesanton@icloud.com>
2018-08-09 20:35:31 -0400

    feat(server): added API routes

---
+++ server/api/users.spec.js
@@ -0,0 +1,32 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const User = db.model('user')
+
+describe('User routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/users/', () => {
+    const userEmail = 'default@email.com'
+
+    beforeEach(() => {
+      return User.create({
+        email: userEmail
+      })
+    })
+
+    it('GET /api/users', async () => {
+      const res = await request(app)
+        .get('/api/users')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].email).to.be.equal(userEmail)
+    })
+  }) // end describe('/api/users')
+}) // end describe('User routes')
