5db86a7ebe1191267865518e11ae714b7659090c
Anton Holmes <holmesanton@icloud.com>
2018-08-15 13:45:46 -0400

    feat(boilerplate): completed until bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: [
-    '@babel/polyfill',
+    '@babel/polyfill', // enables async-await
     './client/index.js'
   ],
   output: {
@@ -24,3 +24,30 @@ module.exports = {
     ]
   }
 }
+
+// const isDev = process.env.NODE_ENV === 'development'
+
+// module.exports = {
+//   mode: isDev ? 'development' : 'production',
+//   entry: [
+//     '@babel/polyfill',
+//     './client/index.js'
+//   ],
+//   output: {
+//     path: __dirname,
+//     filename: './public/bundle.js'
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+//   devtool: 'source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader'
+//       }
+//     ]
+//   }
+// }
